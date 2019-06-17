4089726096c5c9b2f123983b3b49fa30c19d9cfb
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 17:49:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,20 @@
 # Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-def speak_to_grandma(phrase) {
-  if phrase != phrase.upcase
-    return "HUH!? SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
   end
-}
-# If you shout, she can hear you (or at least she thinks so)
+end
+  
+
+# Whatever you say to grandma, she should respond with
+# HUH?! SPEAK UP, SONNY!
+# unless you shout it (type in all capitals).
+
+# If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
 # NO, NOT SINCE 1938!
