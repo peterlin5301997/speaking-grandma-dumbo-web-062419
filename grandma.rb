de4958953bd35961f577485f4c8496cb76743e06
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 17:49:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,13 +8,13 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   end
 end
-  
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
